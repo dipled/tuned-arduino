dd69bdba894b2599d89d3262114d69e86840c789
Pedro Vargas Tannuri <87032838+pe91@users.noreply.github.com>
2022-12-15 07:29:36 -0300

    Update morse.ino
    Removed useless comments from former code used

--- morse.ino
+++ morse.ino
@@ -1,21 +1,11 @@
-/*
-  Blink
- Turns on an LED on for one second, then off for one second, repeatedly.
 
- This example code is in the public domain.
- */
-
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 int led = 8;
 int led2 = 8;
 
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(led, OUTPUT); 
   char *s = morsefy("teste");
    interpret(s);  
@@ -25,7 +15,6 @@ void loop(){
 
 }
 
-// the loop routine runs over and over again forever:
 
 
 char* morsefy(char *s){
@@ -120,7 +109,7 @@ void interpret(char* s){
   int h = 300;
   for(int i = 0; i < strlen(s); i += 1){
     if(s[i] == '.'){
-      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(led, HIGH);  
       delay(h);
     }
 
